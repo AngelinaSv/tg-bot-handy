9da99e687495267a91f27b715ba3a5beead96237
AngelinaSv <anna97asakura@gmail.com>
2025-08-25 15:25:32 +0300

    chore: update database connection SSL config

--- db.js
+++ db.js
@@ -3,10 +3,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = {
